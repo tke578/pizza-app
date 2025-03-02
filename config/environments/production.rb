f5b816c0eb5ac61d81f774d67f39b0f46ce7cc79
Andrew Garcia <andrew.garcia@nikolamotor.com>
2025-03-02 16:08:54 -0700

    turn off active storage service for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files in Tigris Global Object Storage (see config/storage.yml for options).
-  config.active_storage.service = :tigris
+  # config.active_storage.service = :tigris
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
